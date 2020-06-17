45cda10d9970aa91a3b31b165bc3ade2607848cc
Hiyokokeko <sukepon0914@gmail.com>
2020-06-17 21:20:08 +0900

    user model, users controller 作成
    ・docker-compose exec web rails g controller users new create
    ・docker-compose exec web rails g model user
    ・docker-compose exec web rails db:migrate
    ・docker-compose up で再起動

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,22 @@
 class UsersController < ApplicationController
+  # GET /users/new
   def new
+    @user = User.new
+  end
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render action: "new"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
 end
