b659baebc667045fbf5be2aca3efc508c9808c91
Hiyokokeko <sukepon0914@gmail.com>
2020-07-01 22:06:34 +0900

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_19_051629) do
+ActiveRecord::Schema.define(version: 2020_07_01_130005) do
+
+  create_table "pages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_pages_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_pages_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -22,4 +32,5 @@ ActiveRecord::Schema.define(version: 2020_06_19_051629) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "pages", "users"
 end
