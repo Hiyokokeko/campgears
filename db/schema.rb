d443affe0ef35005a9d63ac1e46299a0ae122329
Hiyokokeko <sukepon0914@gmail.com>
2020-09-29 11:03:37 +0900

    ページテーブルに写真カラムの追加 rails db:migrate
    docker-compose exec web rails g migration add_picture_to_pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_20_055147) do
+ActiveRecord::Schema.define(version: 2020_09_01_025612) do
 
   create_table "pages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_08_20_055147) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
     t.index ["user_id", "created_at"], name: "index_pages_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_pages_on_user_id"
   end
