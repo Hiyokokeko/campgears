3570158c0d177f1670494ae224cd565f3c63ccc0
Hiyokokeko <sukepon0914@gmail.com>
2020-09-29 12:31:55 +0900

    ページモデル追加

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,3 +1,14 @@
 class Page < ApplicationRecord
   belongs_to :user
+  # バリデーション
+  validates :user_id, presence: true
+  validates :title, presence: true, length: { maximum: 60 }
+  validates :content, presence: true, length: { maximum: 800 }
+  validate  :picture_size
+  # 画像投稿
+  mount_uploader :picture, PictureUploader
+  # カテゴリー分け（中間テーブルに対する関連付け）
+  has_many :page_category_relations, dependent: :destroy
+  # 中間テーブルを介するcategoryテーブルへの関連付け
+  has_many :categories, through: :page_category_relations
 end
