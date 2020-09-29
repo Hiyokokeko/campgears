914e15674374f9313d245beb9360809b9dd32714
Hiyokokeko <sukepon0914@gmail.com>
2020-09-29 13:16:34 +0900

    rspecエラーのため一時削除

--- app/models/page.rb
+++ app/models/page.rb
@@ -5,10 +5,4 @@ class Page < ApplicationRecord
   validates :title, presence: true, length: { maximum: 60 }
   validates :content, presence: true, length: { maximum: 800 }
   validate  :picture_size
-  # 画像投稿
-  mount_uploader :picture, PictureUploader
-  # カテゴリー分け（中間テーブルに対する関連付け）
-  has_many :page_category_relations, dependent: :destroy
-  # 中間テーブルを介するcategoryテーブルへの関連付け
-  has_many :categories, through: :page_category_relations
 end
