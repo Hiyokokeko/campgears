a8f13cc988a32a62e2d7af6575fd3adb78fb260e
Hiyokokeko <sukepon0914@gmail.com>
2020-10-01 10:23:08 +0900

    新規投稿の画像関連（編集中）
    設定と表示されるまで

--- app/models/page.rb
+++ app/models/page.rb
@@ -5,4 +5,14 @@ class Page < ApplicationRecord
   validates :title, presence: true, length: { maximum: 60 }
   validates :content, presence: true, length: { maximum: 800 }
   validate  :picture_size
+
+  # 画像投稿
+  mount_uploader :picture, PictureUploader
+
+  private
+
+  # アップロードされた画像のサイズをバリデーションする
+  def picture_size
+    errors.add(:picture, '5MB以下である必要があります') if picture.size > 5.megabytes
+  end
 end
