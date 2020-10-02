c721d16600650097af9f6728343713501b3ee4a9
Hiyokokeko <sukepon0914@gmail.com>
2020-10-02 10:43:19 +0900

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_01_015416) do
+ActiveRecord::Schema.define(version: 2020_10_02_013248) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "page_category_relations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "page_id"
+    t.bigint "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_page_category_relations_on_category_id"
+    t.index ["page_id"], name: "index_page_category_relations_on_page_id"
   end
 
   create_table "pages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -42,5 +51,7 @@ ActiveRecord::Schema.define(version: 2020_10_01_015416) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "page_category_relations", "categories"
+  add_foreign_key "page_category_relations", "pages"
   add_foreign_key "pages", "users"
 end
