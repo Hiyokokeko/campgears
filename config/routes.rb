4fbc05aff014b17c50b4da5e0178cf1bea68dbff
Hiyokokeko <sukepon0914@gmail.com>
2020-11-17 23:58:05 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,16 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  get '/rank',       to: 'pages#rank'
   post '/guest_login', to: 'users#guest_login'
   resources :users do
+    member do
+      get :like_pages
+    end
   end
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :pages do
     resources :comments, only: [:create, :destroy]
+    resources :likes, only: [:create, :destroy]
   end
 end
