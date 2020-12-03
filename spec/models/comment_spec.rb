6f31d75f962b46ef163e4840c423fe38a7f5e4f8
Hiyokokeko <sukepon0914@gmail.com>
2020-12-03 20:40:42 +0900

    コメントモデルテスト

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,33 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Comment, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Comment, type: :model do
+  let(:user) { create(:user) }
+  let(:page) { create(:page) }
+  let(:comment) { create(:comment) }
+
+  it 'user, page, contentがあれば投稿できる' do
+    comment = Comment.new(
+      user: user,
+      page: page,
+      content: 'my string'
+    )
+    expect(comment).to be_valid
+  end
+
+  it 'contentがなければ投稿できない' do
+    comment.content = ''
+    comment.valid?
+    expect(comment.errors).to be_added(:content, :blank)
+  end
+
+  it '140文字以下のコメントは投稿できる' do
+    comment.content = 'a' * 140
+    expect(comment).to be_valid
+  end
+
+  it '141文字以上のコメントは投稿できない' do
+    comment.content = 'a' * 141
+    comment.valid?
+    expect(comment.errors).to be_added(:content, :too_long, count: 140)
+  end
+end
